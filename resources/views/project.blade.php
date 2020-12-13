e240788dffbde4295b31547c72257c5d7a51d861
jerem <jeremie.cyrille@icloud.com>
2020-12-13 13:14:30 +0100

    feature
    add github instead of using a db

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -9,6 +9,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/project.css') }}" rel="stylesheet">
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+        <link href="{{ asset('github-widget\github-widget.css') }}" rel="stylesheet" type="text/css" />
         <title>Jérémie CYRILLE</title>
     </head>
     <body>
@@ -30,39 +31,8 @@
                 </ul>
             </div>
           </nav>
-          <section id="work" class="portfolio-mf sect-pt4 route"> 
-          <div class="row"> 
-           @foreach($Project as $key => $value)    
-        <div class="col-md-4">        
-          <div class="work-box">
-            <a href="{{ asset('img/IMG_0237.jpeg') }}" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="{{ asset('img/IMG_0237.jpeg') }}" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">{{ $value->title }}</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Web Design</span> / <span class="w-date">{{ $value->create_date }}</span>
-                    </div>
-                    <div class="w-more">
-                    {{ $value->description }}
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-       @endforeach 
-      </div>
-      
-          </section>
+          <div class="github-widget" data-user="jcyrille972"></div>
+          <script src="{{ asset('github-widget/github-widget.min.js') }}"></script>
+
 </body>
 </html>     
